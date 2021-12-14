e6a6b0a7ca3afbaa0aa4917131d75d7c702494dd
gozixq <natsinee2545@gmail.com>
2021-12-14 15:50:13 +0700

    ooo

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -21,3 +21,21 @@ void printO(int N,int M){
     else cout << "Invalid input";
  	
 }
+int main(){
+
+	printO(2,2);
+	cout << "\n";
+
+	printO(3,5);
+	cout << "\n";	
+
+	printO(5,3);
+	cout << "\n";	
+
+	printO(0,3);
+	cout << "\n";	
+
+	printO(7,-1);
+	cout << "\n";	
+
+	return 0;
